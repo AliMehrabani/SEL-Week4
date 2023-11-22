b982d960b7a9035a62514a02d10f7b1d61984c8d
AliMehrabani <ali1379m@gmail.com>
2023-11-22 22:02:00 +0330

    Add DeliveryStateContext class without implementation.

---
+++ src/main/java/DeliveryStateContext.java
@@ -0,0 +1,24 @@
+public class DeliveryStateContext {
+    private DeliveryState currentState;
+    private ShippingType currentType;
+
+    public DeliveryStateContext() {
+
+    }
+
+    public void setCurrentState(DeliveryState currentState) {
+
+    }
+
+    public void setCurrentType(ShippingType currentType) {
+
+    }
+
+    public String getDeliveryState() {
+        return "";
+    }
+
+    public String getShippingType() {
+        return "";
+    }
+}
